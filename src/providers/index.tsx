ab5e3b3eca23df298b5496b4393f250ca5e7e542
nkumar0101 <nkumar0101@berkeley.edu>
2026-03-04 18:07:56 -0500

    added traces

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,6 +1,5 @@
 import { AuthProvider } from '@/providers/Auth'
 import { EcommerceProvider } from '@payloadcms/plugin-ecommerce/client/react'
-import { stripeAdapterClient } from '@payloadcms/plugin-ecommerce/payments/stripe'
 import React from 'react'
 
 import { HeaderThemeProvider } from './HeaderTheme'
@@ -35,9 +34,12 @@ export const Providers: React.FC<{
               },
             }}
             paymentMethods={[
-              stripeAdapterClient({
-                publishableKey: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY || '',
-              }),
+              {
+                name: 'mock',
+                label: 'Demo Checkout',
+                initiatePayment: true,
+                confirmOrder: true,
+              },
             ]}
           >
             {children}
